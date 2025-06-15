08a0a72a5d2fa840609f5787e72bcbca4be5cde2
Sashank Pisupati <sashank.pisupati@gmail.com>
2025-06-15 15:50:54 +0200

    amendment to system prompt

--- annotator/agent.py
+++ annotator/agent.py
@@ -5,7 +5,7 @@ from annotator._logfire_mcp import (
     get_logfire_records_schema,
 )
 from annotator.models import _model
-from annotator.tools import load_span, load_trace, save_trace_data
+from annotator.tools import load_span, load_trace, save_trace_data, summarize_critiques, summarize_trace
 
 agent = CodeAgent(
     model=_model,
@@ -15,6 +15,8 @@ agent = CodeAgent(
         save_trace_data,
         arbitrary_query,
         get_logfire_records_schema,
+        summarize_trace,
+        summarize_critiques
     ],
     stream_outputs=True,
     additional_authorized_imports=["json"],
@@ -29,20 +31,28 @@ agent.prompt_templates["system_prompt"] = (
     + """
     You are an expert in annotating and evaluating agent execution traces.
     The user will provide you with a trace ID or a verbose description of a trace query they want to run.
-    You will then use the trace_downloader agent to download the trace data and store it in a file.
-    You will then use the trace_annotator agent to annotate the trace data.
-    You will then return a summary and critique of the trace to the user.
+    You will then use the get_logfire_records_schema, arbitrary_query, and save_trace_data tools to download the trace data and store it in a file.
+    You will then use the load_trace and summarize_trace tools to summarize the trace data.
+    You will then use load_span and critique_span to critique each span, and then use summarize_critiques to summarize the critiques.
+    You will then return a trace analysis report to the user with the given format.
+    
+    Format:
+        # Trace analysis report
+        ## Executive summary
+        ## Critique-annotated conversation summary
 
     You should only perform the actions that each of the tools allows you to do. For example,
     do not start to evaluate or annotate the traces outside of the trace annotator tool and vice versa.
 
     You should take the following steps to evaluate and annotate the trace.
-    1. Download the trace data from logfire using the trace_downloader agent.
-        1.1 The result of this agent should be a pickle file containing the trace data.
+    1. Download the trace data from logfire using the get_logfire_records_schema, arbitrary_query tools.
+        1.1 The result of this agent should be a JSON file containing the trace data, save this file using the save_trace_data tool.
     2. Annotate the trace data using the trace_annotator agent.
-        2.1 The pickle file should first be loaded from the file we have just created in step 1.
-        2.2 The entire trace should be broken down into spans, and each span summarized and critiqued.
-        2.3 Finally, span level summaries and critiques should be summarized into trace level summaries and critiques
-    3. Return the final summary and critique to the user.
+        2.1 The JSON data should first be loaded from the file we have just created in step 1 using the load_trace tool.
+        2.2 The entire trace should be summarized using the summarize_trace tool.
+        2.3 The trace should be broken down into spans and formatted for the critique_span tool.
+        2.4 The list of critiques should be summarized using the summarize_critiques tool.
+    3. Compile your findings from the above into a trace analysis report - where the executive summary contains 
+        a high level overview, and the annotated conversation summary contains a walkthrough of the trace with key critiques flagged.
     """
 )
