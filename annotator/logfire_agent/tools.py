c32e4926c1585f2ec60ce9488309cba6c4c2d1ca
jake golden <jake@atla-ai.com>
2025-06-15 14:54:38 +0200

    Jake (#7)
    * merge
    
    * push

---
+++ annotator/logfire_agent/tools.py
@@ -0,0 +1,36 @@
+
+import json
+import os
+from typing import Any
+
+from logfire.experimental.query_client import LogfireQueryClient
+from smolagents import tool
+
+data_dir_path = os.path.join(os.path.dirname(__file__), "..", "data")
+
+logfire_client = LogfireQueryClient(
+    os.getenv("LOGFIRE_READ_TOKEN"),
+    os.getenv("LOGFIRE_BASE_URL"),
+)
+
+@tool
+def save_trace_data(trace_id: str, trace_data: list[dict[str, Any]]) -> str:
+    """Save trace data to a JSON file in the data/ directory.
+
+    The trace data is saved to a JSON file in the data/ directory with path:
+    `data/<trace_id>.json`.
+
+    Trace data can be obtained via the `arbitrary_query` tool.
+
+    Args:
+        trace_id: The trace ID to save the data for.
+        trace_data: The trace data to save.
+
+    Returns:
+        The path to the file where the trace data is saved.
+    """
+    os.makedirs(os.path.join(data_dir_path), exist_ok=True)
+    file_path = os.path.join(data_dir_path, f"{trace_id}.json")
+    with open(file_path, "w") as f:
+        json.dump(trace_data, f)
+    return file_path
