52535a02590b9488e6bdba87438a61d720186e6f
Sashank Pisupati <sashank.pisupati@gmail.com>
2025-06-15 14:30:21 +0200

    updated arch

--- annotator/agents.py
+++ annotator/agents.py
@@ -4,29 +4,52 @@ from annotator.logfire_tools import arbitrary_query, get_logfire_records_schema
 from annotator.models import _model
 from annotator.tools import span_critique
 
-span_critic = ToolCallingAgent(
+trace_downloader = CodeAgent(
     model=_model,
     tools=[
-        span_critique
-    ],
-    verbosity_level=1,
-    max_tool_threads=10,
-    name="span_critic",
-    description="This agent critiques individual spans in parallel in an agent execution trace."
-)
-
-orchestrator = CodeAgent(
-    model=_model,
-    tools = [
-        arbitrary_query,
         get_logfire_records_schema,
-    ],
-    managed_agents = [
-        span_critic
+        arbitrary_query,
+        # save trace
     ],
     stream_outputs=True,
     verbosity_level=1,
     planning_interval=1,
+    name="trace_downloader",
+    description="This agent queries logfire for a trace with a given traceID, and saves it to a JSON file."
+
+)
+
+
+trace_annotator = CodeAgent(
+    model=_model,
+    tools=[
+        # load_trace,
+        # summarize,
+        # critique,
+    ],
+    verbosity_level=1,
+    max_tool_threads=10,
+    stream_outputs=True,
+    name="trace_annotator",
+    description="""This agent loads a trace from a JSON file, uses tools to summarize and critique a trace.
+    It does so by first breaking the trace into spans (each with their own conversation history).
+    For each span, get a summary & critique of that span.
+    Then, get a summary of all the summaries & all the critiques.
+    Return this summary and critique of the whole trace.
+    """
+)
+
+orchestrator = CodeAgent(
+    model=_model,
+    managed_agents = [
+        trace_downloader,
+        trace_annotator
+    ],
+    stream_outputs=True,
+    verbosity_level=1,
+    planning_interval=12,
     name="trace_annotator",
     description="This agent analyzes and annotates agent execution traces."
 )
+
+
